dcb72040ce018a40634195a4fb5fe711310423a0
Mark Staebler <markstaebler@gmail.com>
2017-02-24 13:45:33 -0800

    node modules added

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.listen(process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.json(req.headers);
+});
